cc20974f5a1da74c80b61bb084562cd22dccb263
Furkan ACAR <furkan@acar.digital>
2019-08-13 19:38:33 +0300

    FooterMenu added.

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,5 +1,6 @@
 //#region Global Imports
 import React from 'react';
+import Link from 'next/link';
 //#endregion Global Imports
 import './style.scss';
 
@@ -7,8 +8,35 @@ import './style.scss';
 import { IFooter } from '@Interfaces';
 //#endregion Interface Imports
 
+type MenuLink = {
+	name: string;
+	path: string;
+}
+
+const footerMenu: MenuLink[] = [
+	{ name: 'Home', path: '/' },
+	{ name: 'Terms and Conditions', path: '/' },
+	{ name: 'Privacy Policy', path: '/' },
+	{ name: 'Collection Statement', path: '/' },
+	{ name: 'Help', path: '/' },
+	{ name: 'Manage Account', path: '/' }
+]
+
+
 export const Footer = (props: IFooter.IProps): JSX.Element => (
-	<div className="Footer">
-		<span>Footer</span>
-	</div>
+	<footer>
+		<div className="footer__menu">
+			{footerMenu.map(({name, path}, i) => (
+				<div className="menu-item" key={i}>
+					<Link href={path}>
+						<a>{name}</a>
+					</Link>
+				</div>
+			))}
+		</div>
+
+		<div className="footer__copyright"></div>
+
+		<div className="footer__backlinks"></div>
+	</footer>
 );
