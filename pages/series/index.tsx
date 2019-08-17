cdec6837d61dbde5e29839ecb03683969f49395c
Furkan ACAR <furkan@acar.digital>
2019-08-18 01:40:11 +0300

    Container element for Series is ready.

--- pages/series/index.tsx
+++ pages/series/index.tsx
@@ -5,35 +5,56 @@ import { bindActionCreators, Dispatch } from 'redux';
 //#endregion Global Imports
 
 //#region Local Imports
+import { Presentational } from './presentational';
+import { MoviesActions } from '@Actions';
+import { GridItem } from '@Components';
+import { filter, sort, preparePageData } from '../../src/Helpers/PageHelpers';
 import './style.scss';
 //#endregion Local Imports
 
 //#region Interface Imports
-import { ISeries } from '@Interfaces';
+import { ISeries, IStore } from '@Interfaces';
 //#endregion Interface Imports
 
 class Series extends React.Component<ISeries.IProps, ISeries.IState> {
-
 	constructor(props: ISeries.IProps) {
 		super(props);
-
-		this.state = {
-		};
 	}
+
+	componentDidMount() {
+		const { FetchJSON, series } = this.props;
+
+		if (series && !series.length) FetchJSON();
+	}
+
+	renderSeries = () => {
+		const { series, filterOptions } = this.props;
+
+		if (!series) return;
+
+		const filtered = filter(series, filterOptions);
+		const sorted = sort(filtered, filterOptions);
+
+		return preparePageData(sorted).map(({ title, url }, i) => (
+			<GridItem key={i} desc={title} img={url} />
+		));
+	};
 
 	public render(): JSX.Element {
 		return (
-			<div className="Series">
-				Series
-			</div>
+			<Presentational {...this.props}>
+				{this.renderSeries()}
+			</Presentational>
 		);
 	}
 }
 
-const mapStateToProps = state => state;
+const mapStateToProps = (state: IStore) => state.movies;
 
-const mapDispatchToProps = (dispatch: Dispatch) => (
-);
+const mapDispatchToProps = (dispatch: Dispatch) =>
+	bindActionCreators(MoviesActions, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(Series);
-
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps,
+)(Series);
