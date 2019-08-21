6d54fa7635dedcbcbafe1079108218b75056a789
Furkan ACAR <furkan@acar.digital>
2019-08-21 13:39:45 +0300

    Toast Tested.

--- src/Components/Toast/index.spec.tsx
+++ src/Components/Toast/index.spec.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import { Toast } from '../Toast';
+import * as React from 'react';
+import { mount, shallow } from 'enzyme';
+import { Toast } from '@Components';
+import { IToast } from '@Interfaces';
+
+const loadingProps: IToast.IProps = {
+	isLoading: true,
+	err: false,
+};
+
+const errorProps: IToast.IProps = {
+	isLoading: false,
+	err: true,
+};
 
 describe('Components', () => {
-    describe('Toast', () => {
-        
-    })
-})
+	describe('Toast', () => {
+		it('should load successfully', () => {
+			const wrap = shallow(<Toast {...loadingProps} />);
+
+			expect(wrap.find('h4').text()).toBe('Loading');
+			expect(wrap.find('.quote').exists()).toBeTruthy();
+		});
+
+		it('should errors in a decent way', () => {
+			const wrap = mount(<Toast {...errorProps} />);
+
+			expect(wrap.find('.error').exists()).toBeTruthy();
+			expect(wrap.find('h4').text()).toBe('Error');
+		});
+	});
+});
